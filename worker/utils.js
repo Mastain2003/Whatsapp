182e5fd3f9aec85841bd076ba754ff6ee409da33
Mastain2003 <prakharmastain9@gmail.com>
2026-07-23 23:18:34 +0530

    Update utils.js

--- worker/utils.js
+++ worker/utils.js
@@ -1,1 +1,51 @@
+export async function checkAuth(
+    request,
+    env
+){
 
+    const auth =
+    request.headers.get("Authorization");
+
+
+    if(!auth){
+        return false;
+    }
+
+
+    const token =
+    auth.replace("Bearer ","");
+
+
+    try{
+
+        const parts =
+        token.split(".");
+
+
+        if(parts.length !== 3){
+            return false;
+        }
+
+
+        const payload =
+        JSON.parse(
+            atob(parts[1])
+        );
+
+
+        if(payload.exp < Date.now()){
+            return false;
+        }
+
+
+        return true;
+
+
+    }
+    catch(e){
+
+        return false;
+
+    }
+
+}
