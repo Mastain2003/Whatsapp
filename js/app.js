d7f40fb51e950c3f2be1709f831b9e8b5b6e7422
Mastain2003 <prakharmastain9@gmail.com>
2026-07-23 12:15:36 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,90 @@
-console.log("WhatsApp Order Manager started");
+let customers = [];
+function showPage(page){
+
+    document.getElementById("dashboard").style.display="none";
+    document.getElementById("customers").style.display="none";
+
+    document.getElementById(page).style.display="block";
+
+}
+
+
+
+function uploadExcel(){
+
+    let file =
+    document.getElementById("excelFile").files[0];
+
+
+    if(!file){
+        alert("Select Excel file");
+        return;
+    }
+
+
+    let reader = new FileReader();
+
+
+    reader.onload=function(e){
+
+        let data =
+        new Uint8Array(e.target.result);
+
+
+        let workbook =
+        XLSX.read(data,{type:"array"});
+
+
+        let sheet =
+        workbook.Sheets[workbook.SheetNames[0]];
+
+
+        customers =
+        XLSX.utils.sheet_to_json(sheet);
+
+
+        displayCustomers();
+
+    };
+
+
+    reader.readAsArrayBuffer(file);
+
+}
+
+
+
+function displayCustomers(){
+
+    let table =
+    document.getElementById("customerTable");
+
+
+    table.innerHTML="";
+
+
+    customers.forEach(c=>{
+
+
+        table.innerHTML += `
+
+        <tr>
+
+        <td>${c.Name || ""}</td>
+
+        <td>${c.Phone || c.Mobile || ""}</td>
+
+        <td>${c.Email || ""}</td>
+
+        </tr>
+
+        `;
+
+
+    });
+
+
+    document.getElementById("customerCount").innerText =
+    customers.length;
+
+}
