054513779b009ea873622051caf57bdaab4b5692
Mastain2003 <prakharmastain9@gmail.com>
2026-07-24 22:12:03 +0530

    Update page_login.js

--- public/js/page_login.js
+++ public/js/page_login.js
@@ -94,7 +94,7 @@ async function(){
             setTimeout(()=>{
 
                 window.location.href =
-                "customers.html";
+                "dashboard.html";
 
             },800);
 
