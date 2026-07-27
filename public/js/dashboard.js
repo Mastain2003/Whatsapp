582364598866e0846abc5fa6853553d71ec2181c
Mastain2003 <prakharmastain9@gmail.com>
2026-07-27 11:05:40 +0530

    Update dashboard.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,50 @@
 import {
     requireLogin,
-    logout
+    apiFetch
 } from "./core.js";
-import { loadSidebar } from "./sidebar.js";
+
+import {
+    loadSidebar
+} from "./sidebar.js";
 
 requireLogin();
+
 loadSidebar("dashboard");
 
+loadDashboard();
 
+async function loadDashboard(){
+
+    const result =
+    await apiFetch(
+        "/dashboard"
+    );
+
+    if(
+        !result ||
+        !result.success
+    ){
+        return;
+    }
+
+    document.getElementById(
+        "customerCount"
+    ).innerText =
+    result.dashboard.customers;
+
+    document.getElementById(
+        "productCount"
+    ).innerText =
+    result.dashboard.products;
+
+    document.getElementById(
+        "messageCount"
+    ).innerText =
+    result.dashboard.messages;
+
+    document.getElementById(
+        "broadcastCount"
+    ).innerText =
+    result.dashboard.broadcasts;
+
+}
