0aa1c278ab4470d7680acb37d2ccb93619569945
Mastain2003 <prakharmastain9@gmail.com>
2026-07-27 10:57:53 +0530

    Update api_products.js

--- worker/api_products.js
+++ worker/api_products.js
@@ -13,13 +13,13 @@ export async function handleProducts(
     env
 ){
 
-    const authorized =
+    const user =
     await checkAuth(
         request,
         env
     );
 
-    if(!authorized){
+    if(!user){
 
         return jsonResponse(
             {
