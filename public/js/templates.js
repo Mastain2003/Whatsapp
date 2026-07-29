1b24be1cc53a54b0e79110344bfd1a07ee5dba60
Mastain2003 <prakharmastain9@gmail.com>
2026-07-29 23:27:09 +0530

    Update templates.js

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,1 +1,37 @@
+import {
+    requireLogin,
+    apiFetch
+} from "./core.js";
 
+import {
+    loadSidebar
+} from "./sidebar.js";
+
+requireLogin();
+
+loadSidebar("templates");
+
+init();
+
+async function init(){
+
+    try{
+
+        const result =
+        await apiFetch(
+            "/templates"
+        );
+
+        console.log(result);
+
+    }
+    catch(error){
+
+        console.error(
+            "Unable to load templates",
+            error
+        );
+
+    }
+
+}
