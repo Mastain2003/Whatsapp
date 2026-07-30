62490798ed953bd6ece1bddd168cd47f4d6da59a
Mastain2003 <prakharmastain9@gmail.com>
2026-07-31 00:57:31 +0530

    Update api_customers.js

--- worker/api_customers.js
+++ worker/api_customers.js
@@ -15,7 +15,6 @@ import {
 
 
 function generateCustomerCode(id) {
-
     return "CUS" +
         String(id)
         .padStart(6, "0");
@@ -70,208 +69,78 @@ export async function handleCustomers(
 
     // GET CUSTOMERS
 
-    if(
-        method === "GET"
-    ){
-
-
-        let query = `
-    SELECT *
-    FROM customers
-`;
-
-let conditions = [];
-let values = [];
-
-
-const name =
-    url.searchParams.get("name");
-
-const city =
-    url.searchParams.get("city");
-
-const department =
-    url.searchParams.get("department");
-
-const phone =
-    url.searchParams.get("phone");
-
-
-
-if(name){
-
-    conditions.push(
-        "name LIKE ?"
-    );
-
-    values.push(
-        `%${name}%`
-    );
-
-}
-
-
-
-if(city){
-
-    conditions.push(
-        "city LIKE ?"
-    );
-
-    values.push(
-        `%${city}%`
-    );
-
-}
-
-
-
-if(department){
-
-    conditions.push(
-        "department LIKE ?"
-    );
-
-    values.push(
-        `%${department}%`
-    );
-
-}
-
-
-
-if(phone){
-
-    conditions.push(
-        "phone LIKE ?"
-    );
-
-    values.push(
-        `%${phone}%`
-    );
-
-}
-
-
-
-if(conditions.length > 0){
-
-    query +=
-    " WHERE "
-    +
-    conditions.join(
-        " AND "
-    );
-
-}
-
-
-
-query +=
-`
-ORDER BY id DESC
-`;
-
-
-
-const result =
-    await env.DB
-    .prepare(query)
-    .bind(...values)
-    .all();
-
-
+    if(method === "GET"){
+        let query = `SELECT * FROM customers`;
+        let conditions = [];
+        let values = [];
+        const name =url.searchParams.get("name");
+        const designation =url.searchParams.get("designation");
+        const department =url.searchParams.get("department");
+        const city =url.searchParams.get("city");
+        const block =url.searchParams.get("block");
+        const phone =url.searchParams.get("phone");
+        
+        if(name){
+            conditions.push("name LIKE ?");
+            values.push(`%${name}%`);
+        }
+        if(designation){
+            conditions.push("designation LIKE ?");
+            values.push(`%${designation}%`);
+        }
+        if(department){
+            conditions.push("department LIKE ?");
+            values.push(`%${department}%`);
+        }
+        if(city){
+            conditions.push("city LIKE ?");
+            values.push(`%${city}%`);
+        }if(block){
+            conditions.push("block LIKE ?");
+            values.push(`%${block}%`);
+        }
+        if(phone){
+            conditions.push("phone LIKE ?");
+            values.push(`%${phone}%`);
+        }
+        if(conditions.length > 0){
+            query += " WHERE "+ conditions.join(" AND ");
+        }
+        query += `ORDER BY id DESC`;
+        
+        const result = await env.DB.prepare(query).bind(...values).all();
 
         return jsonResponse({
-
             success:true,
-
             customers:
             result.results
-
         });
-
-
     }
-
-
-
-
-
-
-
 
     // ADD CUSTOMER
 
-    if(
-        method === "POST"
-    ){
-
-
-        const body =
-            await request.json();
-
-
-
-
-        const insert =
-            await env.DB
-            .prepare(
-                `
-                INSERT INTO customers
-                (
-                    customer_code,
-                    name,
-                    designation,
-                    department,
-                    city,
-                    phone
+    if(method === "POST"){
+        const body = await request.json();
+        const insert = await env.DB.prepare(               `
+                INSERT INTO customers(
+                   customer_code,name,designation,department,city,block,phone
                 )
-                VALUES
-                (
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?,
-                    ?
+                VALUES(
+                    ?,?,?,?,?,?,?
                 )
                 `
-            )
-            .bind(
-
-                "TEMP",
-
-                body.name,
-
-                body.designation,
-
-                body.department,
-
-                body.city,
-
-                body.phone
-
-            )
-            .run();
+            ).bind(
+                "TEMP",body.name,body.designation,body.department, body.city,body.block,body.phone
+            ).run();
 
 
 
 
 
-        const id =
-            insert.meta.last_row_id;
-
-
-
-
-        const code =
-            generateCustomerCode(id);
-
-
-
-
-
+        const id =  insert.meta.last_row_id;
+        
+        const code = generateCustomerCode(id);
+        
         await env.DB
         .prepare(
             `
