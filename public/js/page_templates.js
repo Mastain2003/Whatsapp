7ff73e55081ea2ee2c8f789440d7574354aded4b
Mastain2003 <prakharmastain9@gmail.com>
2026-07-30 16:35:26 +0530

    Update page_templates.js

--- public/js/page_templates.js
+++ public/js/page_templates.js
@@ -573,15 +573,7 @@ function renderPreview(
 
                     `{{${index+1}}}`,
 
-                    `<span
-                    style="
-                    color:#0a7cff;
-                    font-weight:bold;
-                    ">
-
-                    ${value}
-
-                    </span>`
+                    `<span style="color:#0a7cff;font-weight:bold;">${value}</span>`
 
                 );
 
