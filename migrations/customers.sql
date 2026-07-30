3992d6bab8d1d9a15ed560ba0152cf62f89edf25
Mastain2003 <prakharmastain9@gmail.com>
2026-07-31 00:09:26 +0530

    Update customers.sql

--- migrations/customers.sql
+++ migrations/customers.sql
@@ -1,1 +1,13 @@
-CREATE TABLE customers ( id INTEGER PRIMARY KEY AUTOINCREMENT, customer_code TEXT UNIQUE NOT NULL, name TEXT NOT NULL, designation TEXT, department TEXT, city TEXT, phone TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP, updated_at DATETIME DEFAULT CURRENT_TIMESTAMP )
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  customer_code TEXT UNIQUE NOT NULL, 
+  name TEXT NOT NULL, 
+  designation TEXT, 
+  department TEXT, 
+  city TEXT, 
+  block TEXT,
+  phone TEXT UNIQUE NOT NULL,
+  whatsapp_language TEXT DEFAULT "en_IN",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  )
