f14ddb9a1942ec8594ebf4b0590113c2b2a42f8c
Mastain2003 <prakharmastain9@gmail.com>
2026-09-19 12:01:59 +0530

    Update sidebar.js

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -4,7 +4,7 @@ import {
 
 export async function loadSidebar(activePage){
 
-    const container =
+   const container =
     document.getElementById(
         "sidebarContainer"
     );
@@ -21,7 +21,7 @@ export async function loadSidebar(activePage){
     }
 
 
-    const response =
+   /* const response =
     await fetch(
         "../components/sidebar.html"
     );
@@ -32,7 +32,7 @@ export async function loadSidebar(activePage){
 
 
     container.innerHTML =
-    html;
+    html;*/
 
 
 
